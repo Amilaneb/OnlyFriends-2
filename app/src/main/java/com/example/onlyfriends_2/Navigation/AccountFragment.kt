c12d97f0ca7a41b73d466a2876becbb463e55ac1
Alexandre Deluc <alexandre.deluc07@gmail.com>
2022-03-07 17:56:50 +0100

    Chabgement pdp

--- app/src/main/java/com/example/onlyfriends_2/Navigation/AccountFragment.kt
+++ app/src/main/java/com/example/onlyfriends_2/Navigation/AccountFragment.kt
@@ -1,16 +1,18 @@
 package com.example.onlyfriends_2.Navigation
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.onlyfriends_2.R
+import com.example.onlyfriends_2.EditProfilActivityFragmentInteraction
 import com.example.onlyfriends_2.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
     lateinit var binding: FragmentAccountBinding
+    private var interactor: EditProfilActivityFragmentInteraction?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +20,20 @@ class AccountFragment : Fragment() {
     ): View? {
         binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
+        }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.editBtn.setOnClickListener {
+            interactor?.showEditProfil()
+        }
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        interactor = context as? EditProfilActivityFragmentInteraction
+    }
+
 }
+
